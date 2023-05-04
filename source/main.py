90dd03590913859ea0509c8cf0ba6fe0c1e9412a
ayaan <seconddate@naver.com>
2023-05-04 16:05:20 +0900

    add start shell

--- source/main.py
+++ source/main.py
@@ -25,4 +25,4 @@ async def read_item(skip: int = 0, limit: int = 10):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+    uvicorn.run("main:app", port=port, reload=False)
