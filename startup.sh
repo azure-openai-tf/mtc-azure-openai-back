781e4164dcdd7206cba353e71956e7250136767c
ayaan <ayaan@mz.co.kr>
2023-05-11 17:04:16 +0900

    .

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 
 echo GCC Lib Install Start
+apt update
 apt -y install build-essential gcc
 echo GCC Lib Install End
 
 echo PIP Install Start
 python -m pip install -r requirements.txt
 echo PIP Install END
-
 
 echo Start Server
 python -m uvicorn api:app --host 0.0.0.0 --app-dir source
