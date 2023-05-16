fcee7d3f523f1febbe16cf62d5192fa928fbd236
ayaan <seconddate@naver.com>
2023-05-16 13:09:23 +0900

    test

--- source/api.py
+++ source/api.py
@@ -3,6 +3,7 @@
 @created_at 2023.05.08
 """
 import random
+import test
 from fastapi import FastAPI, UploadFile, status, Request
 from fastapi.responses import JSONResponse
 from custom_exception import APIException
@@ -202,22 +203,7 @@ async def query_chatbot(chatbot_query: ChatbotQuery):
 @app.get("/test", status_code=status.HTTP_200_OK, tags=["Test"])
 async def search():
     """test"""
-    return {
-        "users": [
-            {
-                "userId": 1,
-                "firstName": "AAAAA",
-                "lastName": "as23",
-                "phoneNumber": "123456",
-                "emailAddress": "AAAAA@test.com",
-                "homepage": "https://amogg.tistory.com/1",
-            },
-            {"userId": 2, "firstName": "BBBB", "lastName": "h5jdd", "phoneNumber": "123456", "homepage": "https://amogg.tistory.com/2"},
-            {"userId": 3, "firstName": "CCCCC", "lastName": "2dhbs", "phoneNumber": "33333333", "homepage": "https://amogg.tistory.com/3"},
-            {"userId": 4, "firstName": "DDDDD", "lastName": "bacasd", "phoneNumber": "222222222", "homepage": "https://amogg.tistory.com/4"},
-            {"userId": 5, "firstName": "EEEEE", "lastName": "asdfasdf", "phoneNumber": "111111111", "homepage": "https://amogg.tistory.com/5"},
-        ]
-    }
+    return test.test()
 
 
 @app.get("/chatbot/search", status_code=status.HTTP_200_OK, tags=["ChatGPT"])
