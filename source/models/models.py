41f579e04ede8c0272603e4320d1ab9e48b25ac6
hudson <hava12@naver.com>
2023-05-16 14:37:04 +0900

    DB 작업

--- source/models/models.py
+++ source/models/models.py
@@ -23,17 +23,5 @@ class DeleteBlobsBody(BaseModel):
 class ChatbotQuery(BaseModel):
     """ChatbotQuery"""
     
-    def __init__(self):
-        self.engine = create_engine(DB_URL, pool_recycle = 500)
-
-    def sessionmaker(self):
-        Session = sessionmaker(bind=self.engine)
-        session = Session()
-        return session
-
-    def connection(self):
-        conn = self.engine.connect()
-        return conn   
-    
     query: str
     messages: list = []
