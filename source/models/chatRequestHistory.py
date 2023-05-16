a61eacabaf4e8556b66053177a78989479a23d25
hudson <hava12@naver.com>
2023-05-16 15:04:25 +0900

    변수명 camel case to snake case로 변경

--- source/models/chatRequestHistory.py
+++ source/models/chatRequestHistory.py
@@ -10,15 +10,28 @@ class ChatRequestHistory(Base):
     __tablename__ = 'chat_request_history'
 
     id = Column("id", INT, nullable=False, autoincrement=True, primary_key=True)
-    selectedIndex = Column("selected_index", TEXT)
-    requestAt = Column("request_at", DATETIME(timezone=True))
-    requestMessage = Column("request_message", TEXT, default='')
-    responseAt = Column("response_at", DATETIME(timezone=True))
-    responseMessage = Column("response_message", TEXT, default='')
-    responseCode = Column("response_code", TEXT, default='')
-    referenceFile = Column("reference_file", TEXT, default='')
-    createAt = Column("create_at", DATETIME(timezone=True), default=func.now())
-    createUser = Column("create_user", TEXT, default='')
-    updateAt = Column("update_at", DATETIME(timezone=True))
-    updateUser = Column("update_user", TEXT)
+    selected_index = Column("selected_index", TEXT)
+    request_at = Column("request_at", DATETIME(timezone=True))
+    request_message = Column("request_message", TEXT, default='')
+    response_at = Column("response_at", DATETIME(timezone=True))
+    response_message = Column("response_message", TEXT, default='')
+    response_code = Column("response_code", TEXT, default='')
+    reference_file = Column("reference_file", TEXT, default='')
+    create_at = Column("create_at", DATETIME(timezone=True), default=func.now())
+    create_user = Column("create_user", TEXT, default='')
+    update_at = Column("update_at", DATETIME(timezone=True))
+    update_user = Column("update_user", TEXT)
     
+def openAiRequestToModelRequest(params):
+    print(params)
+    result = {
+
+    }
+    return ""
+
+def openAiResponseToModelResponse(params):
+    print(params)
+    result = {
+
+    }
+    return ""
