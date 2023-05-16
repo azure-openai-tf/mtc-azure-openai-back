38a7f98e70224a2d37a82f7bef3d2f428b657bcd
hudson <hava12@naver.com>
2023-05-16 15:07:53 +0900

    MYSQL 접속정보 환경변수 적용

--- source/models/database.py
+++ source/models/database.py
@@ -1,8 +1,13 @@
 from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
+import os
 
+azure_mysql_username = os.environ.get("AZURE_MYSQL_USER")
+azure_mysql_password = os.environ.get("AZURE_MYSQL_PASSWORD")
+azure_mysql_db = os.environ.get("AZURE_MYSQL_NAME")
+azure_mysql_host = os.environ.get("AZURE_MYSQL_HOST")
 
-DB_URL = 'mysql://openaiAdmin:@.mysql.database.azure.com:3306/openai'
+DB_URL = 'mysql://{azure_mysql_username}:{azure_mysql_password}@{azure_mysql_host}/{azure_mysql_db}'
 class engineconn:
 
     def __init__(self):
